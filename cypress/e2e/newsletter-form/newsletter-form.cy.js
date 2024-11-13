aefa830f82b33eb7ace03b72d38485c1e42a54cf
Reander Agulto <reander.agulto@oqulo.com>
2024-11-13 11:11:07 +0800

    Newsletter Form Test

---
+++ cypress/e2e/newsletter-form/newsletter-form.cy.js
@@ -0,0 +1,37 @@
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    return false
+});
+
+import { 
+    siteLink
+} from '../../support/helpers/global-helper';
+
+import {    
+    getTestInfo
+} from '../../support/helpers/forms';
+
+describe('Loft Single Page Banner Form Testing', () => {
+    // Visit Specific Page for Testing
+    it(`Loft Single Page Banner Form Test ${siteLink.url}`, () => {
+        cy.visit(siteLink.url, {
+            auth: siteLink.auth
+        });
+        cy.get('form.site-form.newsletter-form').then((forms) => {
+            cy.get(forms).each((form) => {
+                cy.wrap(form).within(() => {
+                    if(form.find('input[type="text"], input[type="checkbox"], input[type="tel"], input[type="email"], select, textarea').length > 0) {
+                        cy.get(form.find('input[type="text"], input[type="checkbox"], input[type="tel"], input[type="email"], select, textarea')).each(($input) => {
+                            getTestInfo($input);
+                        })
+                    }
+                    if(form.find('input[type="submit"]').length > 0) {
+                        cy.get('input[type="submit"]').click({force: true});
+                    }
+                });
+            });
+        });
+    });
+    
+});
